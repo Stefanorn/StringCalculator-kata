f2c68b7a5889103e79a05f0d4847129471c94b6d
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-09 09:20:39 +0000

    implemented a care to thrown if user puts in negative numbers

--- src/calculator.js
+++ src/calculator.js
@@ -8,6 +8,11 @@ function add(number){
 		var totalSum = 0;
 		number = number.split(pattern);
 		for( var i = 0; i < number.length; i++){
+
+			if(parseInt(number) < 0){
+				throw "Negatives not allowed: " + number[i];
+			}
+
 			totalSum += parseInt(number[i]);
 		}
 		return totalSum;

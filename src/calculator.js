2b6f05e86998a5c2747e0369cbefd87de95e7bbd
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-10 19:35:18 +0000

    Overview some final test and refactorin

--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,16 @@
 function add(number){
 
+	// If user inputs a emty string i will return 0
 	if(number == ""){
 		return 0;
 	}
 
-	var negativeNumberMask = /-\d*/g;
-	if( negativeNumberMask.test(number) ){
+	// Checks if the string contains a negative numver
+	// And throws an error
+	var negativeNumberPatern = /-\d*/g;
+	if( negativeNumberPatern.test(number) ){
 		var errorString = "Negatives not allowed: ";
-		number = number.match(negativeNumberMask);
-
+		number = number.match(negativeNumberPatern);
 		for(var i = 0; i < number.length; i++){
 			errorString += number[i] +", "
 		}
@@ -16,27 +18,36 @@ function add(number){
 		throw errorString;
 	}
 
+	// Checks if the strings contains a number greater than
+	// 1000 and replaces that string with zero
 	var maxNumbersPattern = /\d\d\d\d\d*/g
 	number = number.replace(maxNumbersPattern,"0");
 
-	var pattern = /[,\n]/;
+	// Declair the pattern witch i look for to split up the string
+	// and checks if user whants to use their custom spliter
+	// and adds that token to the pattern.
+	var splitUpNumberPattern = /[,\n]/;
 	if(number.includes("\\\\")){
 		var inputMask = /..(.+).n(.*)/g;		
 		var CaptureGroup = inputMask.exec(number);
 		var newSplitToken = CaptureGroup[1];
-		pattern = new RegExp('[,\\n' + newSplitToken + ']' );
+		splitUpNumberPattern = new RegExp('[,\\n' + newSplitToken + ']' );
 		number = CaptureGroup[2];
 	}
-	if( pattern.test(number) ){
+
+	// Here i sum up the string and return a int
+	if( splitUpNumberPattern.test(number) ){
 		var totalSum = 0;
 		
-		number = number.split(pattern);
+		number = number.split(splitUpNumberPattern);
 		for( var i = 0; i < number.length; i++){
 			totalSum += parseInt(number[i]);
 		}
 		return totalSum;
 	}
-
+	
+	// If the useer only inputs a single number and nothing else
+	// than this is the final return condition	
 	return parseInt(number);
 }
 
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -12,29 +12,24 @@ it("Should return 6", () => {
 it("Should return 9", () => {
 	expect(add("3,3,3")).toBe(9);
 });
-
 it("Should return 9", () => {
 	expect(add("1,1,1,1,1,1,1,1,1")).toBe(9);
 });
-
 it("Should return 8", () => {
 	expect(add("1,1\n1,1\n1,1\n1,1")).toBe(8);
 });
-
 it("Should throw an error with Error code 'Negatives not allowed: -1'", () => {
 	function tryAdd (){
 		add("-1,2");
 	}
 	expect(tryAdd).toThrowError("Negatives not allowed: -1");
 });
-
 it("Should throw an error with Error code 'Negatives not allowed: -2, -3'", () => {
 	function tryAdd (){
 		add("-2,2,-3");
 	}
 	expect(tryAdd).toThrowError("Negatives not allowed: -2, -3");
 });
-
 it("Should throw an error with Error code 'Negatives not allowed: -2, -3, -43, -2'", () => {
 	function tryAdd (){
 		add("-2,2\n-3,-43,3\n2,-2");
@@ -56,14 +51,15 @@ it("Should return 5", () => {
 it("Should return 5", () => {
 	expect(add("999,5\n1000")).toBe(1004);
 });
-
+it("Should return 5", () => {
+	expect(add("5,1000,1000,1000")).toBe(5);
+});
 it("Should throw an error with Error code 'Negatives not allowed: -1, -1000", () => {
 	function tryAdd (){
 		add("1\n-1\n-1000");
 	}
 	expect(tryAdd).toThrowError("Negatives not allowed: -1, -1000");
 });
-
 it("Should throw an error with Error code 'Negatives not allowed: -1, -1000", () => {
 	function tryAdd (){
 		add("\\\\;/n5;-10,10");
